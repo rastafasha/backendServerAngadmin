019c985d93a010c1af1693bad08d82617e7f8998
Malcolm Cordova <mercadocreativo@gmail.com>
2019-10-10 12:06:52 -0400

    finalizado

--- routes/login.js
+++ routes/login.js
@@ -13,6 +13,21 @@ var CLIENT_ID = require('../config/config').CLIENT_ID;
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(CLIENT_ID);
 
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+
+// autenticacion de token
+app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res) => {
+
+    var token = jwt.sign({ usuario: req.usuario }, SEED, { expiresIn: 14400 }); // 4 horas= 14400 / 60
+
+    res.status(200).json({
+        ok: true,
+        usuario: req.usuario,
+        token: token
+    });
+
+})
 
 // autenticacion de google
 async function verify(token) {
